8bde496b4c77dcf21797a068ba051f552f408750
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-08 16:06:53 -0800

    wrote varibles for current date and time

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+myStorage = window.localStorage;
+
+var hour = moment().format('h:mm:ss a')
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var userInput;
+var hourSpan;
