9ba9f2b2a46209ff9e2622fe372d9fbe0d665b89
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-08 16:15:25 -0800

    created vars for each hour using moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,14 @@ var hour = moment().format('h:mm:ss a')
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var userInput;
 var hourSpan;
+
+var hour9 = $("#9:00am");
+var hour10 = $("#10:00am");
+var hour11 = $("#11:00am");
+var hour12 = $("#12:00pm");
+var hour13 = $("#1:00pm");
+var hour14 = $("#2:00pm");
+var hour15 = $("#3:00pm");
+var hour16 = $("#4:00pm");
+var hour17 = $("#5:00pm");
+var hour18 = $("#6:00pm");
